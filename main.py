6b54ba847194d8e367117f182ef52ddef2eb39c9
yarik <yaroslav.sotnikov.05@bk.ru>
2022-04-03 16:56:20 +0300

    Начало кода потом добавлю базу данных с пользователями и их информацией + статистика игры

--- main.py
+++ main.py
@@ -2,15 +2,98 @@ import logging
 import pretty_errors
 import random
 
-from telegram.ext import Updater
+# t.me/yl_game_bot
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
 
 # логгинг
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
 logger = logging.getLogger(__name__)
+
+GENDER, AGE, REGION = range(3)
+
+FLAG = True
+
+dict_user_infrom = {'gender': '', 'age': '', 'region': ''}
+
+
+def start(update, context):
+    user = update.message.from_user
+    logger.info(f'Пользователь {user.first_name} начал беседу с ботом.')
+    reply_keyboard = [['Mужчина', 'Женщина', 'Другое']]
+    update.message.reply_text(
+        'Привет, меня зовут Арханвей. Я бот который будет играть\n'
+        'с тобой в игру. Ты погрузишься и средневековье, где\n'
+        'будут замки, рыцари и драконы. Но для начала\n'
+        'я задам тебе пару вопросов.\n\n'
+        'Какой у тебя пол?',
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True,
+            input_field_placeholder='Впиши свой пол'
+        )
+    )
+
+    return GENDER
+
+
+def gender(update, context):
+    user = update.message.from_user
+    logger.info(f'Пол пользователя {user.first_name}: {update.message.text}.')
+    update.message.reply_text(
+        'Хорошо! А теперь скажи пожалуйста сколько тебе лет',
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+    return AGE
+
+
+def age(update, context):
+    user = update.message.from_user
+    logger.info(f'Возраст {user.first_name}: {update.message.text}.')
+    update.message.reply_text(
+        'Остался последний вопрос из какой ты страны'
+    )
+
+    return REGION
+
+
+def region(update, context):
+    user = update.message.from_user
+    logger.info(f'{user.first_name} написал что он из {update.message.text}.')
+    update.message.reply_text(
+        'ок'
+    )
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info(f"Пользователь {user.first_name} закончил беседу с ботом.")
+    update.message.reply_text(
+        'Пока! Надеюсь мы скоро встретимся.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
 
 
 def main():
     """!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"""
-    updater = Updater('TOKEN')
+    updater = Updater('5294956202:AAGngiDjlf7FdpeQqkYgn6eZXhuIdpetqJM')
     dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={GENDER: [MessageHandler(Filters.regex('^(Mужчина|Женщина|Другое)$'), gender)],
+                AGE: [MessageHandler(Filters.text, age)],
+                REGION: [MessageHandler(Filters.text, region)]
+                },
+        fallbacks=[CommandHandler('cancel', cancel)]
+
+    )
+    dp.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
